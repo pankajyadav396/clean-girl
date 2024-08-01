3b2154bf2d96f472d662039f9a76909f602aae70
Your Name <you@example.com>
2024-08-01 13:55:36 +0530

    commit meta tags and data description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>clean-girl</title>
+        <meta name="title" content="clean-girl" />
+        <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="clean-girl" />
+        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        <meta property="og:image" content="https://i.ibb.co/c3FrL6z/Screenshot-2024-07-29-144000.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="clean-girl" />
+        <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        <meta property="twitter:image" content="https://i.ibb.co/c3FrL6z/Screenshot-2024-07-29-144000.png" />
+
+        {/* <!-- Meta Tags Generated with https://metatags.io --> */}
+      </head>
       <body>{children}</body>
     </html>
   );
